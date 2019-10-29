b2684140ea45c8584a4d8dc235fff8e2e1153883
tumit <tumit@live.com>
2019-10-29 17:45:08 +0700

    refactor: move header, footer and jumbotron to layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./layouts/header/header.component";
+import { FooterComponent } from "./layouts/footer/footer.component";
+import { JumbotronComponent } from "./layouts/jumbotron/jumbotron.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, JumbotronComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
